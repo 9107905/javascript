4064ca919a30c7bfe9c552bc82540c2a9d47b9e9
Giorgos Marmarinos <9026101@student.zadkine.nl>
2024-11-06 16:13:05 +0100

    Add files via upload

---
+++ opdracht 11/app.js
@@ -0,0 +1,32 @@
+function multiply() {
+    if(num1.value > 0 && num2.value>0){
+        let ans = num1.value * num2.value;
+        answer.innerText = ans;
+    } else {
+        answer.innerText = "getal is te laag"
+    }
+}
+function divide() {
+    if(num1.value > 0 && num2.value>0) {
+        let ans = num1.value / num2.value;
+        answer.innerText = ans;
+    } else {
+        answer.innerText = "getal is te laag";
+    }
+}
+function add() {
+    if(num1.value > 0 && num2.value>0) {
+        let ans = num1.value + num2.value;
+        answer.innerText = ans;
+    } else {
+        answer.innerText = "getal is te laag";
+    }
+}
+function minus() {
+    if(num1.value > 0 && num2.value>0) {
+        let ans = num1.value - num2.value;
+        answer.innerText = ans;
+    } else {
+        answer.innerText = "getal is te laag";
+    }
+}
